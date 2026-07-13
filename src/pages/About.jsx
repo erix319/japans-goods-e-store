a0145a3705a1f225137fa3f889dedb9393eb4048
George Pavlov <bill_gatez@bk.ru>
2026-07-13 07:22:39 +0300

    Updates

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import readmeContent from '../../README.md?raw';
-
-const About = () => {
-	return <ReactMarkdown>{readmeContent}</ReactMarkdown>;
-};
-
-export default About;
